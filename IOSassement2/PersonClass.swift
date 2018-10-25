21f3e8c27dd2ea2dac333b0667559fa8fcd43158
Jacob Tassinari <jacobtassinari@Interapts-MacBook-Pro-3.local>
2018-10-25 10:59:18 -0500

    finshed project

---
+++ IOSassement2/PersonClass.swift
@@ -0,0 +1,20 @@
+//
+//  PersonClass.swift
+//  IOSassement2
+//
+//  Created by Jacob Tassinari on 10/25/18.
+//  Copyright © 2018 Jacob Tassinari. All rights reserved.
+//
+
+import Foundation
+
+class Person {
+    var firstName: String
+    var lastName: String
+    var Email: String
+    init(firstName:String,lastName:String,Email:String) {
+        self.firstName = firstName
+        self.lastName = lastName
+        self.Email = Email
+    }
+}
--- IOSassement2/ViewController.swift
+++ IOSassement2/ViewController.swift
@@ -9,10 +9,43 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    //mark:Properties
+    @IBOutlet var FirstText: UITextField!
+    @IBOutlet var LastText: UITextField!
+    @IBOutlet var EmailText: UITextField!
+    
+    //mark: Button
+    @IBAction func SubmitTapped(_ sender: Any) {
+        guard
+            let first = FirstText.text, !first.isEmpty,
+            let last = LastText.text, !last.isEmpty,
+            let email = EmailText.text, !email.isEmpty
+            else{
+                //mark: error alert
+                let errorAlert = UIAlertController(title: "Error", message: "Please fill out all of the fields.", preferredStyle: UIAlertController.Style.alert)
+                let dismissAction = UIAlertAction(title: "Ok", style: UIAlertAction.Style.default){UIAlertAction in}
+                errorAlert.addAction(dismissAction)
+                self.present(errorAlert, animated: true, completion: nil)
+                return
+        }
+        // mark: New person
+        let NewPerson = Person(firstName: first, lastName: last, Email: email)
+        
+        // mark: Greetings alert
+        let NewAlert = UIAlertController(title: "Greeting!", message: "Hello, \(NewPerson.firstName) \(NewPerson.lastName)! Your email is \(NewPerson.Email)", preferredStyle: UIAlertController.Style.alert)
+        let dismissAction = UIAlertAction(title: "Ok", style: UIAlertAction.Style.default){UIAlertAction in}
+        NewAlert.addAction(dismissAction)
+        self.present(NewAlert, animated: true, completion: nil)
+        return
+        
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        // mark: tapp gesture to turn off keyboard
+self.view.addGestureRecognizer(UITapGestureRecognizer(target: self.view, action: #selector(UIView.endEditing(_:))))
     }
 
 
