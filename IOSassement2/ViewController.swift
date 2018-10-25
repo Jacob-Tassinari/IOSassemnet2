8f5d0b95820bb52c70cde2be3ec09b41207dac77
Jacob Tassinari <jacobtassinari@Interapts-MacBook-Pro-3.local>
2018-10-25 10:00:26 -0500

    Initial Commit

---
+++ IOSassement2/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  IOSassement2
+//
+//  Created by Jacob Tassinari on 10/25/18.
+//  Copyright © 2018 Jacob Tassinari. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
